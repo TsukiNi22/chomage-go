53e3b6188c0a2e27b7c5e4e59aee0cbf680c5295
Salah-Eddine <salah-eddine.ghellab@epitech.eu>
2026-09-01 17:48:27 +0200

    fix(rename): rename companies routes

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,8 +1,16 @@
 import {Router} from "express";
 import authRoutes from "./auth.routes";
+import jobsRoutes from "./jobs.routes";
+import usersRoutes from "./users.routes";
 
 // Init router
 export const router = Router();
 
 // Link routes
 router.use("/auth", authRoutes);
+
+// Jobs routes
+router.use("/", jobsRoutes);
+
+// User routes
+router.use("/users", usersRoutes);
