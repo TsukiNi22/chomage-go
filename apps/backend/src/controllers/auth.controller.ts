73c7c80e58d1d908d39e199c596c86e56193d8c0
Salah-Eddine <salah-eddine.ghellab@epitech.eu>
2026-09-01 14:39:00 +0200

    feat(controller): controller that checks if the validate json works well

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -1,5 +1,49 @@
 import {Request, Response, NextFunction} from "express";
+import {z} from "zod";
+import {validateJson} from "../utils/validateJson.utils";
+import {loginSchema, registerSchema, logoutSchema, meSchema} from "../schemas/auth.schema";
 
-export async function login(req: Request, res: Response, next: NextFunction)
-{
+export async function login(req: Request, res: Response, next: NextFunction) {
+    try {
+        if (!validateJson(loginSchema, req, res)) {
+            return next();
+        }
+    } catch (error) {
+        next(error);
+    }
+
+    next();
+}
+
+export async function register(req: Request, res: Response, next: NextFunction) {
+    try {
+        if (!validateJson(registerSchema, req, res)) {
+            return next();
+        }
+    } catch (error) {
+        next(error);
+    }
+    next();
+}
+
+export async function logout(req: Request, res: Response, next: NextFunction) {
+    try {
+        if (!validateJson(logoutSchema, req, res)) {
+            return next();
+        }
+    } catch (error) {
+        next(error);
+    }
+    next();
+}
+
+export async function me(req: Request, res: Response, next: NextFunction) {
+    try {
+        if (!validateJson(meSchema, req, res)) {
+            return next();
+        }
+    } catch (error) {
+        next(error);
+    }
+    next();
 }
