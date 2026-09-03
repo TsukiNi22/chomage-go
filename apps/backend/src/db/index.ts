aec3d8d46ca633ca54b4ca692e743faf9f9053e6
Tsukini <xartania.contact@gmail.com>
2026-09-03 10:30:17 +0200

    fix(workflow): fix env files setup (try: 3)

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -3,9 +3,7 @@ import {drizzle} from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema.ts";
 
-const url =
-    process.env.DATABASE_URL ||
-    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`;
+const url = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`;
 
 export const client = postgres(url);
 export const db = drizzle(client, {schema});
